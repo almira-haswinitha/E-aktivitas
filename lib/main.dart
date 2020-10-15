7c6a3b8042ea46c6a2bbcf4a2d15ebc501e8dfb2
almira-haswinitha <almirahaswinitha@gmail.com>
2020-10-15 11:13:54 +0700

    hanya 1 halaman yg berisi nama pekerjaan dan tgl mulai selesai

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,147 @@
 import 'package:flutter/material.dart';
-import './hal_status.dart' as status;
-import './hal_tambah.dart' as tambah;
 
-void main() {
-  runApp(new MaterialApp(
-    title: "Tab Bar",
-    home: new Aktivitas(),
-  ));
-}
+void main() => runApp(MyApp());
 
-class Aktivitas extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _AktivitasState createState() => _AktivitasState();
-}
-
-class _AktivitasState extends State<Aktivitas>
-    with SingleTickerProviderStateMixin {
-  TabController controller;
-
-  @override
-  void initState() {
-    controller = new TabController(length: 2, vsync: this);
-    super.initState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Demo",
+      theme: ThemeData(
+          primaryColor: Color(0xFFC41A3B),
+          primaryColorLight: Color(0xFFFBE0E6),
+          accentColor: Color(0xFF1B1F32)),
+      home: MyHomePage(),
+    );
   }
+}
 
+class MyHomePage extends StatefulWidget {
   @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String title = 'Date Picker';
+
+  DateTime _date = DateTime.now();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    DateTime _datePicker = await showDatePicker(
+        context: context,
+        initialDate: _date, // current date
+        firstDate: DateTime(1947), //first date
+        lastDate: DateTime(2030), // last date
+        textDirection:
+            TextDirection.ltr, // header text or button direction ltr or rtl
+        initialDatePickerMode: DatePickerMode.day, // day or year mode
+
+        // button color change and picked color changed
+        builder: (BuildContext context, Widget child) {
+          return Theme(
+            data: ThemeData(
+              primaryColor: Color(0xFFC41A3B),
+              accentColor: Color(0xFFC41A3B),
+            ),
+            child: child,
+          );
+        });
+
+    if (_datePicker != null && _datePicker != _date) {
+      setState(() {
+        _date = _datePicker;
+        print(
+          _date.toString(),
+        );
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new TabBarView(
-        controller: controller,
-        children: <Widget>[
-          new status.Status(),
-          new tambah.Tambah(),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        centerTitle: true,
       ),
-      appBar: new AppBar(
-        title: new Text("E-Aktivitas"),
-        backgroundColor: Colors.amber,
-      ),
-      bottomNavigationBar: new Material(
-        color: Colors.amber,
-        child: new TabBar(
-          controller: controller,
-          tabs: <Widget>[
-            new Tab(
-              text: "Status",
-            ),
-            new Tab(
-              text: "Tambah",
-            )
-          ],
-        ),
+      body: Center(
+        child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: new Column(
+              children: <Widget>[
+                new TextField(
+                  decoration: new InputDecoration(
+                      hintText: "Tambah Pekerjaan",
+                      labelText: "Tambah Pekerjaan",
+                      border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(20.0))),
+                ),
+                new Padding(
+                  padding: new EdgeInsets.only(top: 15.0),
+                ),
+                new TextFormField(
+                  cursorColor: Color(0xFFC41A3B),
+                  readOnly: true,
+                  onTap: () {
+                    setState(() {
+                      _selectDate(context);
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Tanggal mulai',
+                    labelStyle: TextStyle(fontSize: 16.0),
+                    hintText: (_date.toString()),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xFFC41A3B), width: 2.0),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+                new Padding(
+                  padding: new EdgeInsets.only(top: 15.0),
+                ),
+                new TextFormField(
+                  cursorColor: Color(0xFFC41A3B),
+                  readOnly: true,
+                  onTap: () {
+                    setState(() {
+                      _selectDate(context);
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Tanggal mulai',
+                    labelStyle: TextStyle(fontSize: 16.0),
+                    hintText: (_date.toString()),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xFFC41A3B), width: 2.0),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+              ],
+            )),
+
+        // child: Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: <Widget>[
+        //     RaisedButton(
+        //       onPressed: () {
+        //         setState(() {
+        //           _selectDate(context);
+        //         });
+        //       },
+        //       color: Color(0xFFC41A3B),
+        //       child: Text(
+        //         'Data Picker',
+        //         style: TextStyle(color: Colors.white),
+        //       ),
+        //     ),
+        //     Text(_date.toString()),
+        //   ],
+        // ),
       ),
     );
   }
